37f339e66a4e4606247961f02ccf7f0b2ffb2d4b
Noeme Silva <noeme16@gmail.com>
2023-03-13 20:21:43 -0300

    Criando componentes

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,32 +1,29 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <p>
-      For a guide and recipes on how to configure / customize this project,<br>
-      check out the
-      <a href="https://cli.vuejs.org" target="_blank" rel="noopener">vue-cli documentation</a>.
-    </p>
-    <h3>Installed CLI Plugins</h3>
-    <ul>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-babel" target="_blank" rel="noopener">babel</a></li>
-      <li><a href="https://github.com/vuejs/vue-cli/tree/dev/packages/%40vue/cli-plugin-eslint" target="_blank" rel="noopener">eslint</a></li>
-    </ul>
-    <h3>Essential Links</h3>
-    <ul>
-      <li><a href="https://vuejs.org" target="_blank" rel="noopener">Core Docs</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
-      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>
-      <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
-    </ul>
-    <h3>Ecosystem</h3>
-    <ul>
-      <li><a href="https://router.vuejs.org" target="_blank" rel="noopener">vue-router</a></li>
-      <li><a href="https://vuex.vuejs.org" target="_blank" rel="noopener">vuex</a></li>
-      <li><a href="https://github.com/vuejs/vue-devtools#vue-devtools" target="_blank" rel="noopener">vue-devtools</a></li>
-      <li><a href="https://vue-loader.vuejs.org" target="_blank" rel="noopener">vue-loader</a></li>
-      <li><a href="https://github.com/vuejs/awesome-vue" target="_blank" rel="noopener">awesome-vue</a></li>
-    </ul>
+  <div class="Calculadora-1">
+    <div class="calculadora">
+    <h1 class="green">{{ msg }}</h1>
+    <div class="calculadora-simples">
+        <div class="display">{{ number || '0' }}</div>
+        <div @click="clear" class="btn clear">C</div>
+        <div @click="remove" class="btn remove">Del</div>
+        <div @click="append('7')" class="btn">7</div>
+        <div @click="append('8')" class="btn">8</div>
+        <div @click="append('9')" class="btn">9</div>
+        <div @click=" multiplication" class="btn operator">x</div>
+        <div @click="append('4')" class="btn">4</div>
+        <div @click="append('5')" class="btn">5</div>
+        <div @click="append('6')" class="btn">6</div>
+        <div @click="substraction" class="btn operator">-</div>
+        <div @click="append('1')" class="btn">1</div>
+        <div @click="append('2')" class="btn">2</div>
+        <div @click="append('3')" class="btn">3</div>
+        <div @click="addition" class="btn operator">+</div>
+        <div @click="append('0')" class="btn">0</div>
+        <div @click="dot" class="btn">.</div>
+        <div @click="divition" class="btn operator">/</div>
+        <div @click="equal" class="btn operator">=</div>
+      </div>
+  </div>
   </div>
 </template>
 
@@ -35,24 +32,95 @@ export default {
   name: 'HelloWorld',
   props: {
     msg: String
+  },
+  data() {
+    return {
+      prevNum: null,
+      number: '',
+      operator: null,
+      operatorClicked: false
+    }
+  },
+  methods: {
+    clear() {
+      this.number = '';
+    },
+    append(num) {
+      if(this.operatorClicked) {
+        this.number = '',
+        this.operatorClicked = false
+      }
+      console.log(this.append +"erro")
+      this.number = `${this.number}${num}`
+    },
+    dot() {
+      if(this.number.indexOf('.') ===-1) {
+        this.append('.')
+      }
+    },
+    remove() {
+      this.number = this.number.slice(0, -1)
+    },
+    setPrevNum() {
+      this.prevNum = this.number
+      this.operatorClicked = true
+
+    },
+    addition() {
+      this.operator = (a,b) => a + b;
+      this.setPrevNum()
+    },
+    substraction() {
+      this.operator = (a,b) => a - b;
+      this.setPrevNum()
+
+    },
+    multiplication() {
+      this.operator = (a,b) => a * b;
+      this.setPrevNum()
+    },
+    divition() {
+      this.operator = (a,b) => a / b;
+      this.setPrevNum()
+    },
+    equal() {
+      this.number = `${this.operator(
+        parseFloat(this.prevNum),
+        parseFloat(this.number))}`;
+      this.prevNum = '';
+    }
   }
 }
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-h3 {
-  margin: 40px 0 0;
+.calculadora-simples {
+  display: grid;
+  grid-template-columns: repeat(4,1fr);
+  grid-auto-rows: minmax(50px,auto);
+  margin: 0 auto;
+  width: 400px;
+  font-size: 40px;
 }
-ul {
-  list-style-type: none;
-  padding: 0;
+.display {
+  grid-column: 1/5;
+  background-color: #333 !important;
+  color: #fff;
 }
-li {
-  display: inline-block;
-  margin: 0 10px;
+.clear {
+  grid-column: 1/3;
 }
-a {
-  color: #42b983;
+.remove {
+  grid-column: 3/5;
+}
+.btn {
+  background-color: #f2f2f2;
+  border: 1px solid #999;
+  cursor: pointer;
+}
+.operator {
+  background-color: rgb(49,120,148);
+  color: #fff;
 }
 </style>
